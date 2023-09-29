d7b3636c19eab954d95595c3ad45737f2eacd057
vvp43 <vvp43@scarletmail.rutgers.edu>
2023-09-28 23:24:27 -0400

    update compareTo in Event

--- source/src/eventcalendar/Event.java
+++ source/src/eventcalendar/Event.java
@@ -92,8 +92,12 @@ public class Event implements Comparable<Event>{
      */
     @Override
     public int compareTo (Event a) {
-        System.out.println("are dates equal?: "+date.compareTo(a.date));
-        return -40;
+        if(this.date.compareTo(a.date)==0){
+            if(this.startTime.compareTo(a.startTime)==0){
+                return 0;//conflicting scheduling
+            }
+        }
+        return -1;
     }
 
     /**
